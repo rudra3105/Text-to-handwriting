14532de98452cbb9b7bfe8d5fc9b745cb9169977
Rudra Bhatt <105910217+rudra3105@users.noreply.github.com>
2022-07-11 21:32:09 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,3 @@
+import pywhatkit
+
+pywhatkit.text_to_handwriting(" Hi, I'm Rudra Bhatt. I'm pursuing my B. E. Computer Engineering degree at Madhuben And Bhanubhai Institute of Technology", rgb=[0,0,255])
